a54fca5c8124494d9922e418104e2f01fcef9bdf
Danisaur117 <danisaur@gmail.com>
2019-04-11 10:50:18 +0200

    Added README.md

--- src/main/java/com/daniel/belmonte/ApiRestSpring/controllers/ApiController.java
+++ src/main/java/com/daniel/belmonte/ApiRestSpring/controllers/ApiController.java
@@ -1,10 +1,14 @@
 package com.daniel.belmonte.ApiRestSpring.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
 public class ApiController {
-
+	@GetMapping("/hola")
+	public String HolaMundo() {
+		return "Hola mundo";
+	}
 }
